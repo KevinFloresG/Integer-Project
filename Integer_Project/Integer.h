3c1129a99df3e65c3a0dea7a0a21c9b1a6eedfa5
Kevin Flores Garcia <kevinfg1413@gmail.com>
2019-09-11 19:07:08 -0600

    ...

--- Integer_Project/Integer.h
+++ Integer_Project/Integer.h
@@ -4,9 +4,24 @@
 
 #ifndef INTEGER_H
 #define INTEGER_H
+//#define ZERO 0; supuestamente no es recomendado en C++
+//#define ONE 1; supuestamente no es recomendado en C++
+
+#include<iostream>
+
+const short int ZERO = 0;
+const short int ONE = 1;
+
+struct Nodo{
+	unsigned short int* v[5];
+	struct Nodo* next;
+};
 
 class Integer {
-
+private:
+	bool sign; // true->positive || false->negative
+	struct Nodo* first;
+public:
 
 };
 
